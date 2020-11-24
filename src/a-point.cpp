9055b7c538a971cabba58ff889e5c47b6e419135
turing <wfelipet@uni.pe>
2020-11-24 14:35:32 -0500

    1/10

--- src/a-point.cpp
+++ src/a-point.cpp
@@ -1,2 +1,20 @@
 // it displays a point inside a window with Shaders
 
+// cabecera estandar
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+// GLEW (OpenGL Extension Library proveé mecanimos para determinar en run-time qué extensiones de opengl son soportadas) 
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW es una API para el manejo de ventanas.
+#include <GLFW/glfw3.h>
+using namespace std;
+#define numVAOs 1
+
+
+int main(){
+    cout<<"First problem of the first set of exercises about computer graphics";
+    return 0;
+}
