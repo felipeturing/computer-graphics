26bfe503df5bffd7d2d40e11b9a2da86759c72b8
felipeturing <wfelipet@uni.pe>
2020-12-25 15:01:09 -0500

    updating

--- src/practice-2/3/fs.glsl
+++
@@ -1,5 +0,0 @@
-#version 410
-in vec4 vertexColor;
-void main(void){
-    gl_FragColor = vertexColor;
-}
--- src/test-1/4/fs.glsl
+++
@@ -1,6 +0,0 @@
-#version 410
-out vec4 color;
-layout (location = 0) in vec4 vertexColor; // recibe el vertice del Vertex Shader
-void main(void){
-    color = vertexColor;
-}
